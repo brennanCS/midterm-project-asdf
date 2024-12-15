aa6834fb9c71afe82f4ea753c263edd5e507f2dc
Elijah Bentley <48062319+elijabent@users.noreply.github.com>
2024-12-15 21:15:10 +0000

    Creation of 21 Game page, still need to integrate DOM functions and game rules

---
+++ 21.js
@@ -0,0 +1,109 @@
+class Card{
+    constructor(suit, rank, value){
+        this.suit = suit;
+        this.rank = rank;
+        this.value = value;
+        this.image = suit[0].toLowerCase();
+        if (value < 10){
+            this.image += "0" + value;
+        }
+        else if (rank == "10"){
+            this.image += "10";
+        }
+        else if (rank == "Jack"){
+            this.image += "11";
+        }
+        else if (rank == "Queen"){
+            this.image += "12";
+        }
+        else if (rank == "King"){
+            this.image += "13";
+        }
+        else if (rank == "Ace"){
+            this.image += "01";
+        }
+        this.image += ".png";
+    }
+}
+
+let cards = [];
+let playerCards = [];
+let dealerCards = [];
+
+// Spades
+cards.push(new Card("Spades", "Ace", 11));
+cards.push(new Card("Spades", "2", 2));
+cards.push(new Card("Spades", "3", 3));
+cards.push(new Card("Spades", "4", 4));
+cards.push(new Card("Spades", "5", 5));
+cards.push(new Card("Spades", "6", 6));
+cards.push(new Card("Spades", "7", 7));
+cards.push(new Card("Spades", "8", 8));
+cards.push(new Card("Spades", "9", 9));
+cards.push(new Card("Spades", "10", 10));
+cards.push(new Card("Spades", "Jack", 10));
+cards.push(new Card("Spades", "Queen", 10));
+cards.push(new Card("Spades", "King", 10));
+
+// Hearts
+cards.push(new Card("Hearts", "Ace", 11));
+cards.push(new Card("Hearts", "2", 2));
+cards.push(new Card("Hearts", "3", 3));
+cards.push(new Card("Hearts", "4", 4));
+cards.push(new Card("Hearts", "5", 5));
+cards.push(new Card("Hearts", "6", 6));
+cards.push(new Card("Hearts", "7", 7));
+cards.push(new Card("Hearts", "8", 8));
+cards.push(new Card("Hearts", "9", 9));
+cards.push(new Card("Hearts", "10", 10));
+cards.push(new Card("Hearts", "Jack", 10));
+cards.push(new Card("Hearts", "Queen", 10));
+cards.push(new Card("Hearts", "King", 10));
+
+// Diamonds
+cards.push(new Card("Diamonds", "Ace", 11));
+cards.push(new Card("Diamonds", "2", 2));
+cards.push(new Card("Diamonds", "3", 3));
+cards.push(new Card("Diamonds", "4", 4));
+cards.push(new Card("Diamonds", "5", 5));
+cards.push(new Card("Diamonds", "6", 6));
+cards.push(new Card("Diamonds", "7", 7));
+cards.push(new Card("Diamonds", "8", 8));
+cards.push(new Card("Diamonds", "9", 9));
+cards.push(new Card("Diamonds", "10", 10));
+cards.push(new Card("Diamonds", "Jack", 10));
+cards.push(new Card("Diamonds", "Queen", 10));
+cards.push(new Card("Diamonds", "King", 10));
+
+// Clubs
+cards.push(new Card("Clubs", "Ace", 11));
+cards.push(new Card("Clubs", "2", 2));
+cards.push(new Card("Clubs", "3", 3));
+cards.push(new Card("Clubs", "4", 4));
+cards.push(new Card("Clubs", "5", 5));
+cards.push(new Card("Clubs", "6", 6));
+cards.push(new Card("Clubs", "7", 7));
+cards.push(new Card("Clubs", "8", 8));
+cards.push(new Card("Clubs", "9", 9));
+cards.push(new Card("Clubs", "10", 10));
+cards.push(new Card("Clubs", "Jack", 10));
+cards.push(new Card("Clubs", "Queen", 10));
+cards.push(new Card("Clubs", "King", 10));
+
+//shuffles deck
+for (let i = cards.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [cards[i], cards[j]] = [cards[j], cards[i]]; // Swap elements
+}
+
+function dealToPlayer(){
+    let i = Math.floor(Math.random() * cards.length);
+    playerCards.push(cards[i]);
+    cards.splice(i, 1);
+}
+
+function dealToDealer(){
+    let i = Math.floor(Math.random() * cards.length);
+    dealerCards.push(cards[i]);
+    cards.splice(i, 1);
+}
